341113017f12b00c89fd013d49fb5fc7cc250cca
monish <monishanand.j@gmail.com>
2023-03-15 21:11:21 +0530

    leap year

---
+++ flowchart questions/src/Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sum {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a = s.nextInt();
+        System.out.println("enter second number");
+        int b = s.nextInt();
+        int c =a+b;
+        System.out.println("Sum of the numbers is "+c);
+    }
+}
