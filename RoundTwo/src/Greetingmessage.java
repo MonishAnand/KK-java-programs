77ecbb142c8fe554f024eb15a63c30c71deb6a7a
monish <monishanand.j@gmail.com>
2023-03-20 19:25:11 +0530

    message

---
+++ RoundTwo/src/Greetingmessage.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Greetingmessage {
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = c.next();
+        System.out.println("Hi "+name);
+    }
+}
