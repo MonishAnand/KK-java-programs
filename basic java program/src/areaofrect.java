6405f5cbc8684c1c974000c94f5a7be6884e9c79
monish <monishanand.j@gmail.com>
2023-03-31 21:09:18 +0530

    areaofrectangle

---
+++ basic java program/src/areaofrect.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class areaofrect {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter length");
+        double l =s.nextDouble();
+        System.out.println("enter breadth");
+        double b = s.nextDouble();
+        double area=l*b;
+        System.out.println("Area :"+area);
+
+    }
+}
