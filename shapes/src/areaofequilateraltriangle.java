8aa02e640ce8da50404cc1d904cbec8bafbd73e9
monish <monishanand.j@gmail.com>
2023-04-02 17:13:59 +0530

    areaofequilateraltri

--- shapes/src/areaofequilateraltriangle.java
+++ shapes/src/areaofequilateraltriangle.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class areaofequilateraltriangle {
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        System.out.println("enter side");
+        double a=c.nextDouble();
+        double area= (Math.sqrt(3)*(a*a))/4;
+        System.out.println("Area = "+area);
+    }
 }
