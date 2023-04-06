455986bc33a0597ade6d434fc7311e72eef603b6
monish <monishanand.j@gmail.com>
2023-04-06 19:13:59 +0530

    perimeterofrhombus

---
+++ shapes/src/perirhombus.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class perirhombus {
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        System.out.println("enter side :");
+        int a=c.nextInt();
+        int peri= 4*a;
+        System.out.println("Perimeter = "+peri);
+    }
+}
